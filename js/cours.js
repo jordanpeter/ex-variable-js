b8c15d0843eed96ac00cc00295c7e7e6bcaa5143
jordanpeter <jordanpeter26@gmail.com>
2017-12-18 13:52:58 +0100

    save

---
+++ js/cours.js
@@ -0,0 +1,6 @@
+console.log("Bonjour en JavaScript !");
+var métier ='Web Dev';
+var pays = 'Belgique';
+var nom ='Jordan';
+var nbreEnfant= 4;
+console.log("Vous serez " +métier+" et habiterez à "+pays+", et marié a "+nom+" avec "+nbreEnfant+" enfants.")
